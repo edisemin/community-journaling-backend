01a5736ac4fa7fb447b36896f48d7124a3c8fae9
azotamiota <azotamiota@gmail.com>
2022-07-22 19:02:34 +0100

    setup server & tests

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended: false})) // This middleware is needed to get access the data from the frontend html input field values 
+
+
+
+
+
+
+
+
+module.exports = app
