6ee979a71e04fefff8ace8d890a2984f856208fa
azotamiota <azotamiota@gmail.com>
2022-07-22 19:07:42 +0100

    update

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 
 app.use(cors());
 app.use(express.json())
-app.use(express.urlencoded({extended: false})) // This middleware is needed to get access the data from the frontend html input field values 
+app.use(express.urlencoded({extended: false})) // This middleware is needed to get access to the data from the frontend html input field values 
 
 
 
